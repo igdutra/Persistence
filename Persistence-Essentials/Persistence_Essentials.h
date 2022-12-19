cad7deede1d776dc0e3c5155a7173e50b1808e0a
Ivo Dutra <ivogdutra@gmail.com>
2022-12-19 18:45:45 -0300

    Add Project

---
+++ Persistence-Essentials/Persistence_Essentials.h
@@ -0,0 +1,18 @@
+//
+//  Persistence_Essentials.h
+//  Persistence-Essentials
+//
+//  Created by Ivo on 19/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Persistence_Essentials.
+FOUNDATION_EXPORT double Persistence_EssentialsVersionNumber;
+
+//! Project version string for Persistence_Essentials.
+FOUNDATION_EXPORT const unsigned char Persistence_EssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Persistence_Essentials/PublicHeader.h>
+
+
