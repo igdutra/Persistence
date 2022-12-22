0f4345f019b56b10196d5a8151310a4c929f6f67
Ivo Dutra <ivogdutra@gmail.com>
2022-12-22 16:09:53 -0300

    Save Save command requests cache deletion

--- Persistence-EssentialsTests/CacheFeedUseCaseTests.swift
+++ Persistence-EssentialsTests/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import Network
 
  class LocalFeedLoader {
+     let store: FeedStore
+     
      init(store: FeedStore) {
-
+         self.store = store
+     }
+     
+     func save(_ items: [FeedItem]) {
+         store.deleteCachedFeed()
      }
  }
 
  class FeedStore {
      var deleteCachedFeedCallCount = 0
+     
+     func deleteCachedFeed() {
+         deleteCachedFeedCallCount += 1
+     }
  }
 
  class CacheFeedUseCaseTests: XCTestCase {
@@ -25,5 +36,26 @@ import XCTest
 
          XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
      }
+     
+     func test_save_requestsCacheDeletion() {
+         let store = FeedStore()
+         let sut = LocalFeedLoader(store: store)
+
+         sut.save(makeItems())
+         
+         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+     }
 
  }
+
+// MARK: - Helpers
+
+private extension CacheFeedUseCaseTests {
+ 
+    func makeItems() -> [FeedItem] {
+        let uniqueItem1 = FeedItem(id: UUID(), imageURL: anyURL())
+        let uniqueItem2 = FeedItem(id: UUID(), imageURL: anyURL())
+       
+        return [uniqueItem1, uniqueItem2]
+    }
+}
