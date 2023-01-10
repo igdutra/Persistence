0d35c8d1816f1cec72c2a29af2a9ab91b782c5d9
Ivo Dutra <ivogdutra@gmail.com>
2023-01-10 18:32:39 -0300

    Add SaveResult type alias to protect code from potential breaking changes

--- Persistence-EssentialsTests/CacheFeedUseCaseTests.swift
+++ Persistence-EssentialsTests/CacheFeedUseCaseTests.swift
@@ -7,44 +7,7 @@
 
 import XCTest
 import Network
-
-class LocalFeedLoader {
-    private let store: FeedStore
-    private let currentDate: () -> Date
-    
-    init(store: FeedStore, currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    
-    func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [weak self] error in
-            guard let self = self else { return }
-            
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(items, with: completion)
-            }
-        }
-    }
-    
-    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
-         store.insert(items, timestamp: currentDate()) { [weak self] error in
-             guard self != nil else { return }
-
-             completion(error)
-         }
-     }
-}
-
-protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
-}
+import Persistence_Essentials
 
 // MARK: - Tests
 
@@ -124,7 +87,7 @@ class CacheFeedUseCaseTests: XCTestCase {
          let store = FeedStoreSpy()
          var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
 
-         var receivedResults = [Error?]()
+         var receivedResults = [LocalFeedLoader.SaveResult]()
          sut?.save(makeItems()) { receivedResults.append($0) }
 
          sut = nil
@@ -137,7 +100,7 @@ class CacheFeedUseCaseTests: XCTestCase {
          let store = FeedStoreSpy()
          var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
 
-         var receivedResults = [Error?]()
+         var receivedResults = [LocalFeedLoader.SaveResult]()
          sut?.save(makeItems()) { receivedResults.append($0) }
 
          store.completeDeletionSuccessfully()
